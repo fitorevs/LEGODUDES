57d49cab243e2a606f7cde884009f31d6ae48e86
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-01-30 13:56:45 +0100

    components

---
+++ src/components/ContentPage.jsx
@@ -0,0 +1,21 @@
+import Title from './Title'
+import ProductCard from './ProductCard'
+import {products} from '../assets/legodudes'
+
+export default function ContentPage({amount, setAmount, category}){
+    return(
+        <main>
+          <Title category={category}/>
+          {products.map(product => 
+          <ProductCard 
+          key={product.prodid} 
+          category={product.category} 
+          title={product.title} 
+          img={product.imagefile} 
+          price={product.price} 
+          amount={amount} 
+          setAmount={setAmount}
+          /> )}
+        </main>
+    )
+}
