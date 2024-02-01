517d443996062de3234c23327322cfa0e9fe14ea
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-02-01 10:05:18 +0100

    handlevogn

--- src/components/ContentPage.jsx
+++ src/components/ContentPage.jsx
@@ -2,12 +2,15 @@ import Title from './Title'
 import ProductCard from './ProductCard'
 import {products} from '../assets/legodudes'
 
-export default function ContentPage({amount, setAmount, category}){
+export default function ContentPage({amount, setAmount, category, setCart, cart}){
     return(
         <main>
           <Title category={category}/>
           {products.map(product => 
           <ProductCard 
+          cart={cart}
+          setCart={setCart}
+          prodid={product.prodid}
           key={product.prodid} 
           category={product.category} 
           title={product.title} 
